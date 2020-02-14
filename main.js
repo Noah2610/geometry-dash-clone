e7c07a2dec5ca034d3d5e8ad3a9fe13576288a3e
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-02-14 16:41:32 +0100

    Add settings.js, fix typo

--- main.js
+++ main.js
@@ -1,12 +1,5 @@
-const SCREEN_SIZE = {
-    width: 800,
-    height: 400,
-};
-const BG_COLOR = [0, 0, 0];
-
-function setUp() {
+function setup() {
     createCanvas(SCREEN_SIZE.width, SCREEN_SIZE.height);
-
 }
 
 function draw() {
---
+++ settings.js
@@ -0,0 +1,5 @@
+const SCREEN_SIZE = {
+    width: 800,
+    height: 400,
+};
+const BG_COLOR = [0, 0, 0];
