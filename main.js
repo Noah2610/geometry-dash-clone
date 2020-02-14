ec6c02d0ba0948b7de745af75df38bc3543a6c36
MoserLukas2004 <61056008+MoserLukas2004@users.noreply.github.com>
2020-02-14 16:34:49 +0100

    SetUp Canvas p5

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+const SCREEN_SIZE = {
+    width: 800,
+    height: 400,
+}
+
+function setUp() {
+    createCanvas(SCREEN_SIZE.width, SCREEN_SIZE.height);
+
+}
+
+function draw() {
+    background(0, 0, 0);
+}
