ef658b37a94dccf5a40319d0448e140c076fb0d2
MoserLukas2004 <61056008+MoserLukas2004@users.noreply.github.com>
2020-08-11 13:36:01 +0200

    systems.js with all system-functions

--- js/main.js
+++ js/main.js
@@ -44,9 +44,11 @@ function setup() {
 }
 
 function update() {
-    player.update();
-    applyGravity();
-    moveEntities();
+    for(let i = 0; i < entities.length; i++) {
+        player.update();
+        applyGravity();
+        moveEntity(entities[i]);
+    }
 }
 
 function draw() {
@@ -55,104 +57,4 @@ function draw() {
     background(BG_COLOR);
 
     drawEntities();
-}
-
-// Moves all entities (Player, etc.) that,
-// have a position and a velocity.
-function moveEntities() {
-    for (let i = 0; i < entities.length; i++) {
-        const entity = entities[i];
-        if (entity.position && entity.velocity && entity.solid) {
-            let canMove = true;
-            const newposition = {
-                x: entity.position.x,
-                y: entity.position.y,
-            };
-            newposition.x += entity.velocity.x;
-            newposition.y += entity.velocity.y;
-            const tmpEntity = {
-                position: newposition,
-                size: entity.size,
-            };
-            for(let j = 0; j < entities.length; j++) {
-                const entity2 = entities[j];
-                if(entity.id !== entity2.id) {
-                    if(collideBo(tmpEntity, entity2)) {
-                        //console.log("JA");
-                        console.log("Juhu");
-                        //break;
-                    }
-                    if(collideLR(tmpEntity, entity2)) {
-                        //console.log("JA");
-                        console.log("Jap");
-                        //break;
-                    }
-                    if (doEntitiesCollide(tmpEntity, entity2)) {
-                        canMove = false;
-                        entity.velocity.y = 0;
-                        checkJump();
-                        //console.log("Hää"); 
-                        appylGravityAway(); //hier wäre kein gravity
-                        break;
-                    } 
-                }
-            }
-            if (canMove) {
-                entity.position = newposition;
-            }
-        } else if (entity.position && entity.velocity) {
-            entity.position.x += entity.velocity.x;
-            entity.position.y += entity.velocity.y;
-        }
-    }
-}
-
-// Draws all entities as rectangles,
-// that have a position and a size.
-// Draws the entity with a color,
-// if the enity has a color.
-function drawEntities() {
-    for (let i = 0; i < entities.length; i++) {
-        const entity = entities[i];
-        if (entity.position && entity.size) {
-            if (entity.color) {
-                fill(entity.color);
-            }
-            rect(
-                entity.position.x,
-                entity.position.y,
-                entity.size.w,
-                entity.size.h,
-            );
-        }
-    }
-}
-
-// Changes velocity on all entities, that
-// have gravity and velocity.
-// Simulates gravity.
-function applyGravity() {
-    for (let i = 0; i < entities.length; i++) {
-        const entity = entities[i];
-        if (entity.gravity && entity.velocity) {
-            entity.velocity.y += entity.gravity;
-        }
-    }
-}
-
-//neu
-function appylGravityAway() {
-    for (let i = 0; i < entities.length; i++) {
-        const entity = entities[i];
-        if (entity.gravity && entity.gravity) {
-            entity.velocity.y -= entity.gravity;
-        }
-    }
-}
-
-//Player jumps when the button "Space" is pressed
-function checkJump() {
-    if(keyIsDown(32)) { // 32 ... Space
-        entities[0].velocity.y -= 4;
-    }
 }
