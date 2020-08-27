39e6702f055a11e5404ad32f3ab679104cb15daa
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-08-27 03:37:49 +0200

    Settings tweaks

--- js/settings.js
+++ js/settings.js
@@ -4,7 +4,7 @@ const SCREEN_SIZE = {
 };
 const BG_COLOR = [200, 200, 200];
 const FPS = 60;
-const SPEED = 7;
+const SPEED = 6.5;
 const GRAVITY = 0.85;
 const JUMP_VELOCITY = -12;
 const JUMP_ROTATE_STEP = 7;
